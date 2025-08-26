bbf50e440ce600c1a4a73ee3a7e4533ae0f5dd88
olamide kosile <olamidekosile@gmail.com>
2025-08-26 17:17:58 +0100

    Complete SEO optimization - sitemap and robots.txt enhancements

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,13 +1,27 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/', '/api/', '/_next/'],
+        crawlDelay: 1,
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/private/', '/admin/'],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: ['/private/', '/admin/'],
+      },
+    ],
     sitemap: [
       'https://www.tektribe.org.uk/sitemap.xml',
       'https://www.tektribe.com.ng/sitemap.xml',
     ],
+    host: 'https://www.tektribe.org.uk',
   }
 }
